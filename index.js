bb96d45cea8f17adb7a6671d400da8484ba1322d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-05 10:13:56 +0530

    icecream action, reducer, type created

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const redux = require("redux");
 const createStore = redux.createStore;
 const bindActionCreator = redux.bindActionCreators;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 function orderCake() {
   return {
@@ -16,9 +20,22 @@ function restockCake(qty = 1) {
     payload: qty,
   };
 }
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
 
 const initialState = {
   numOfCakes: 10,
+  numOfIceCream: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -33,6 +50,16 @@ const reducer = (state = initialState, action) => {
         ...state,
         numOfCakes: state.numOfCakes + action.payload,
       };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream + action.payload,
+      };
     default:
       return state;
   }
@@ -45,10 +72,14 @@ const unsubscribe = store.subscribe(() => console.log("updated store", store.get
 // store.dispatch(orderCake());
 // store.dispatch(orderCake());
 
-const actions = bindActionCreator({ orderCake, restockCake }, store.dispatch);
+const actions = bindActionCreator({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch);
 
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
-actions.restockCake(5);
+actions.restockCake(3);
+actions.orderIceCream();
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(3);
 // unsubscribe();
