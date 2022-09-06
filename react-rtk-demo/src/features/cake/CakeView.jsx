7d508f58eb53c288828829613b652bbb233c6e0c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-06 09:28:14 +0530

    useDispatch used

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ordered, restocked } from './cakeSlice';
+
 export const CakeView = () =>{
     const numOfCakes = useSelector((state) => state.cake.numOfCakes);
-    
+    const dispatch = useDispatch();
+    const [count, setCount] = useState(1);
   return (
     <div>
       <h2>Number of Cakes - {numOfCakes}</h2>
-      <button>Order Cake</button>
-      <button>Restock Cakes</button>
+      <button onClick={()=>dispatch(ordered())}>Order Cake</button>
+      <input type="number" value={count} onChange={(e)=>setCount(e.target.value)} />
+      <button onClick={()=>dispatch(restocked(parseInt(count)))}>Restock Cakes</button>
     </div>
   )
 }
--- react-rtk-demo/src/features/icecream/IceCreamView.jsx
+++ react-rtk-demo/src/features/icecream/IceCreamView.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-
+import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './iceCreamSlice'
 export default function IceCreamView() {
   const numOfIceCream = useSelector(state=>state.icecream.numOfIceCreams)
+  const dispatch = useDispatch()
+  const [count, setCount] = useState(1);
   return (
     <div>
             <h2>Number of Icecream - {numOfIceCream}</h2>
-      <button>Order Icecream</button>
-      <button>Restock Icecreams</button>
+      <button onClick={()=>dispatch(ordered())}>Order Icecream</button>
+      <input type="number" value={count} onChange={(e)=>setCount(e.target.value)}/>
+      <button onClick={() => dispatch(restocked(parseInt(count)))}>Restock Icecreams</button>
     </div>
   )
 }
